7bcd388d09819b676946c275c3e05afaa4140d11
Dario Kriskovic <dario.kriskovic@digihey.com>
2017-06-28 15:18:08 +0200

    added classNames for favorite icon, and add css when icon active

--- src/components/PlaceItem.js
+++ src/components/PlaceItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Col, Panel } from 'react-bootstrap'
 import Truncate from 'react-truncate'
+import classNames from 'classnames'
 
 const GOOGLE_PLACES_API = process.env.REACT_APP_GOOGLE_PLACES_API
 const GOOGLE_PLACES_KEY = process.env.REACT_APP_GOOGLE_PLACES_KEY
@@ -10,13 +11,23 @@ class PlaceItem extends Component {
 
   render () {
     const { place } = this.props
+    const favoriteIconClasses = classNames(
+      'glyphicon',
+      'glyphicon-heart',
+      'place-box__favorite-icon',
+      { 'place-box__favorite-icon--active': place.isFavorite })
+      
     return (
       <Col xs={12} sm={6} md={3}>
         <Panel className='place-box'>
           <div className='place-box__header' style={{backgroundImage: getBgImage(place.photos)}}>
             <div className='place-box__overlay'></div>
             <h2 className='place-box__title'>{place.name}</h2>
-            <span className="glyphicon glyphicon-heart place-box__favorite-icon" aria-hidden="true" onClick={() => this.props.addToFavorites(place.id)}></span>
+            <span 
+              className={favoriteIconClasses} 
+              aria-hidden="true" 
+              onClick={() => this.props.addToFavorites(place.id)}>
+            </span>
           </div>
           <div className='place-box__content'>
             <span className='place-box__type-icon'
