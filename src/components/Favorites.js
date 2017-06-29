0e7e63860f6677e2763752e2838b30a85162ddb8
Dario Kriskovic <dario.kriskovic@digihey.com>
2017-06-29 10:55:54 +0200

    enable display of favorite places

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import * as actionTypes from '../actions'
 import { PageHeader, Grid } from 'react-bootstrap'
 
+import PlacesList from './PlacesList'
+
 class Favorites extends Component {
+  constructor () {
+    super ()
+
+    this.onFavoriteClick = this.onFavoriteClick.bind(this)
+  }
+
+  onFavoriteClick (place) {
+    this.props.removeFromFavorites(place.id)
+  }
+
   render() {
     return (
       <div>
         <PageHeader bsClass='custom-page-header'>Favorites</PageHeader>
 
         <Grid fluid>
-          fdsfds
+          {this.props.favoritePlaces.length === 0 ? (
+            <div className='content'>
+              <p>No favorite places selected</p>
+            </div>
+          ) : (
+            <PlacesList places={this.props.favoritePlaces} onFavoriteClick={this.onFavoriteClick} />
+          )}
         </Grid>
       </div>
     )
@@ -18,12 +37,15 @@ class Favorites extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
-  return {}
+  return {
+    favoritePlaces: state.favoritePlaces
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    removeFromFavorites: bindActionCreators(actionTypes.removeFromFavorites, dispatch)
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorites)
