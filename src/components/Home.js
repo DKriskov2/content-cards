91938e07eabf74cb60a40d3c7bdc2f55ca18952d
Dario Kriskovic <dario.kriskovic@digihey.com>
2017-06-30 14:09:13 +0200

    fix typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => (
 
     <Grid fluid>
       <div className='content'>
-        <p>This is small app that uses google places public api to find nearby places.</p>
+        <p>This is a small app that uses google places public api to find nearby places.</p>
         <p>On <code>/places</code>page you will be asked to allow access to your geolocation. If denied default location will be used</p>
         <p>Favorite places can be selected and viewed on <code>/favorites</code> page</p>
       </div>
